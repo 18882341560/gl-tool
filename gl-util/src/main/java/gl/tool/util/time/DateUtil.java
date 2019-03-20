4e9c3cc0ec04ef39dcb27b3dc916280f0fe26cc5
gelin <gllovejava@163.com>
2019-03-20 22:40:43 +0800

    切割时间段

--- gl-util/src/main/java/gl/tool/util/time/DateUtil.java
+++ gl-util/src/main/java/gl/tool/util/time/DateUtil.java
@@ -1,9 +1,15 @@
 package gl.tool.util.time;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author gl
@@ -28,4 +34,31 @@ public class DateUtil {
         DateTime dateTime = new DateTime(date);
         return dateTime.toString(pattern);
     }
+
+
+    /**
+     * 切割时间段
+     * @param second 秒
+     * @param startTime 开始时间
+     * @param endTime 结束时间
+     * @return
+     */
+    public static  List<Map<String,String>> cutDateTime(long second, LocalDateTime startTime, LocalDateTime endTime){
+
+        DateTimeFormatter formatter = DateConst.DEFAULT_DATE_TIME;
+        List<Map<String,String>> list = Lists.newArrayList();
+
+        while (startTime.isBefore(endTime)){
+            LocalDateTime plusSeconds = startTime.plusSeconds(second);
+            String formatEnd = formatter.format(plusSeconds);
+            String formatStart = formatter.format(startTime);
+            Map<String,String> map = Maps.newHashMap();
+            map.put("startTime",formatStart);
+            map.put("endTime",formatEnd);
+            list.add(map);
+            startTime = plusSeconds;
+        }
+        return list;
+    }
+
 }
